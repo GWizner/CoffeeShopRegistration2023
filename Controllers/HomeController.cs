7d8f87b91980c339224bf9231a9b0dda46e52ae1
Gregory Wizner <wiznergregory@gmail.com>
2023-04-03 22:07:44 -0400

    Coffee shop registration lab

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Registration() 
+        { 
+            return View(); 
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult HandleSubmit(UserRegistration userRegistration)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Welcome", userRegistration);
+        }
+        public IActionResult Welcome(UserRegistration userRegistration)
+        {
+            return View(userRegistration);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/UserRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Win32;
+
+namespace CoffeeShopRegistration.Models
+{
+    public class UserRegistration
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
